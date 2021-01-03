2a1bf3eef670db1d9e8c3e3701a2ff6a4b03c001
Tomas Cartman <marco12y@outlook.com>
2021-01-03 15:20:59 -0300

    Chapter 24 incomplete (Fetching JSON from Flickr)

---
+++ app/src/main/java/com/dashboarder/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,44 @@
+package com.dashboarder.photogallery
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dashboarder.photogallery.api.FlickrApi
+import retrofit2.*
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
+
+class PhotoGalleryFragment : Fragment() {
+    private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(this, Observer { responseString ->
+            Log.d(TAG, "Response received: $responseString")
+        })
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+        photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
+        photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+
+        return view
+    }
+
+    companion object {
+        fun newInstance() = PhotoGalleryFragment()
+    }
+}
